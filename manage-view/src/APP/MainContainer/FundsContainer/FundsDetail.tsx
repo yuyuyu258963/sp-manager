c87b2c3bab32fffa692d83791da6e9d098ba55ae
Qianyubin12352 <1572358370@qq.com>
2022-12-21 17:01:50 +0800

    fourth submit

--- manage-view/src/APP/MainContainer/FundsContainer/FundsDetail.tsx
+++ manage-view/src/APP/MainContainer/FundsContainer/FundsDetail.tsx
@@ -1,72 +1,114 @@
-import React, { useState }from 'react'
+import React, { useState } from 'react'
 import './FundsDetail.css'
 import { useLocation } from 'react-router-dom';
 import * as echarts from 'echarts';
-// import fundsData from "../../resource/data/foundsData.json"
 import { Button, Col, Row } from 'antd';
 import axios from 'axios';
 
-
+type fund = {
+  "_id": string,
+  "基金全称": string,
+  "基金类型": string,
+  "基金经理": string,
+  "成立日期": string,
+  "成立规模": string,
+  "份额规模": string,
+  "首次最低金额": string,
+  "托管费": string,
+  "基金管理人": string,
+  "最高认购费": string,
+  "最高申购费": string,
+  "最高赎回费": string
+}
 export default function FundsDetail() {
   let location = useLocation();
-  const [fundsData,setFundsData] = useState([])
-  // http://121.4.249.181:5590/
-
-
-  // {
-  //   "_id": "001706",
-  //   "fund_code": "001706",
-  //   "netVal_path": {"2016-09-22": 1.0, "2016-09-23": 1.0, "2016-09-26": 1.0, "2016-09-27": 1.0, "2016-09-28": 1.0, "2016-09-29": 1.0, "2016-09-30": 1.0, "2016-10-10": 1.001, "2016-10-11": 1.001}
-  //     }
-  const foundID: string = location.pathname.split('/').reverse()[0];
+  const [fundsRateData, setFundsData] = useState([])
+  const fundID: string = location.pathname.split('/').reverse()[0];
   axios({
     method: "post",
     url: "/api/trend/net",
-    data: JSON.stringify({"_id": foundID}),
+    data: JSON.stringify({ "_id": fundID }),
     headers: {
       'Content-Type': 'application/json;charset=UTF-8'
     }
   }).then(function (response) {
     //handle success data
     setFundsData(response.data)
-    console.log(fundsData)
   })
     .catch(function (error) {
       //handle error satuation
       console.log(error)
     })
-  
-  // let rankData:any = []
-  // axios({
-  //   method: "post",
-  //   url: "/api/rank",
-  //   data: JSON.stringify({"_id": foundID}),
-  //   headers: {
-  //     'Content-Type': 'application/json;charset=UTF-8'
-  //   }
-  // }).then(function (response) {
-  //   //handle success data
-  //   console.log(response.data)
-  //   rankData = response.data
-  //   console.log(fundsData)
-  // })
-  //   .catch(function (error) {
-  //     //handle error satuation
-  //     console.log(error)
-  //   })
 
+  const [rankData, setRankData] = useState([])
+  axios({
+    method: "post",
+    url: "/api/rank",
+    data: JSON.stringify({ "_id": fundID }),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  }).then(function (response) {
+    //handle success data
+    setRankData(response.data)
+  })
+    .catch(function (error) {
+      //handle error satuation
+      console.log(error)
+    })
 
-  
-  const timeData_top = fundsData.map((founds: any[]) => { return founds[0] })
-  const rateData_top = fundsData.map((founds: any[]) => { return founds[1] })
-  console.log(timeData_top,rateData_top)
-  // 然后净值的形式能不能做成列表 [[time,data],[time,data]]这样
+  const [fund, setFund] = useState<fund>({
+    "_id": '',
+    "基金全称": '',
+    "基金类型": '',
+    "基金经理": '',
+    "成立日期": '',
+    "成立规模": '',
+    "份额规模": '',
+    "首次最低金额": '',
+    "托管费": '',
+    "基金管理人": '',
+    "最高认购费": '',
+    "最高申购费": '',
+    "最高赎回费": ''
+  })
+  axios({
+    method: "post",
+    url: "/api/show/single",
+    data: JSON.stringify({ "_id": fundID }),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  }).then(function (response) {
+    //handle success data
+    setFund(response.data[0]);
+  })
+    .catch(function (error) {
+      //handle error satuation
+      console.log(error)
+    })
+
+  // [ ['2022-09-15', [['3434', '6085', '43.57'], ['321', '7078', '95.46'], ['361', '6994', '94.84'], ['1730', '6795', '74.54'], ['4986', '6473', '22.97'], ['2685', '5512', '51.29'], [], ['2116', '3801', '44.33'], ['1635', '2986', '45.24'], ['611', '2133', '71.35']]]]
+
+  const timeData_top = fundsRateData.map((founds: any[]) => { return founds[0] })
+  const rateData_top = fundsRateData.map((founds: any[]) => { return founds[1] })
+
+  const timeData_bottom: any = [];
+  const rateData_bottom: any = [];
+  for (let i = 0; i < 10; i++) {
+    timeData_bottom.push(rankData.filter((data: any) => { return data[1][i].length > 0 }).map((ranks: any[]) => { return ranks[0] }))
+    rateData_bottom.push(rankData.map((ranks: any[]) => { return ranks[1][i] }))
+  }
   function drawPicture_top() {
     type EChartsOption = echarts.EChartsOption;
     var chartDom = document.getElementById('picture_top')!;
     var myChart = echarts.init(chartDom);
     var option: EChartsOption;
     option = {
+      title: {
+        show: true,
+        text: "净值",
+      },
       xAxis: {
         type: 'category',
         boundaryGap: false,
@@ -81,6 +123,9 @@ export default function FundsDetail() {
           }
         },
       },
+      legend: {
+        show: true,
+      },
       tooltip: {//提示框组件
         trigger: 'axis',
         axisPointer: {
@@ -93,6 +138,7 @@ export default function FundsDetail() {
       },
       series: [
         {
+          name: "净值",
           data: rateData_top,
           type: 'line',
           areaStyle: {
@@ -113,10 +159,14 @@ export default function FundsDetail() {
     var myChart = echarts.init(chartDom);
     var option: EChartsOption;
     option = {
+      title: {
+        show: true,
+        text: "收益排名变化",
+      },
       xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: timeData_top
+        data: timeData_bottom[0].reverse()
       },
       yAxis: {
         type: 'value',
@@ -127,6 +177,9 @@ export default function FundsDetail() {
           }
         },
       },
+      legend: {
+        show: true,
+      },
       tooltip: {//提示框组件
         trigger: 'axis',
         axisPointer: {
@@ -135,11 +188,12 @@ export default function FundsDetail() {
             backgroundColor: '#6a7985'
           }
         },
-        formatter: `{b}<br/> 净值: <span><font color="fba81a">{c}%</font></span>`
+        formatter: `{b}<br/> 收益排名: <span><font color="fba81a">{c}%</font></span>`
       },
       series: [
         {
-          data: rateData_top,
+          name: "收益排名",
+          data: rateData_bottom[0].filter((rate: any) => { return rate.length > 0; }).map((rate: any) => { return rate[2] }).reverse(),
           type: 'line',
           areaStyle: {
             color: "#ecf3fa"
@@ -156,16 +210,16 @@ export default function FundsDetail() {
   React.useEffect(() => {
     drawPicture_top()
     drawPicture_bottom()
-  }, [fundsData])
+  }, [rankData, fundsRateData, fund])
 
   return (
     <div id='FundsDetail'>
       <div className='BaseData'>
         <div className='BaseDataTitle'>
-          <h1 className='FundsName'>FundsName</h1>&nbsp;&nbsp;
-          <span>{foundID}</span>&nbsp;&nbsp;
-          <span className='FoundTips'>&nbsp;&nbsp; FoundTips &nbsp;&nbsp;</span>&nbsp;&nbsp;
-          <span className='FoundRisks'>&nbsp;&nbsp; FoundRisks &nbsp;&nbsp;</span>&nbsp;&nbsp;
+          <h1 className='FundsName'>{fund.基金全称}</h1>&nbsp;&nbsp;
+          <span className='FundsID'>{fund._id}</span>&nbsp;&nbsp;
+          {/* <span className='FoundTips'>&nbsp;&nbsp; FoundTips &nbsp;&nbsp;</span>&nbsp;&nbsp;
+          <span className='FoundRisks'>&nbsp;&nbsp; FoundRisks &nbsp;&nbsp;</span>&nbsp;&nbsp; */}
         </div>
         <div className='BaseDataContainer'>
           <Row>
@@ -183,32 +237,32 @@ export default function FundsDetail() {
                   <div className='d-wfsy'>
                     <p className="value">
                       <b className="percent">
-                        {rateData_top.reverse()[0]}<span>%</span>
+                        {rateData_top[rateData_top.length-1]}<span>%</span>
                       </b>
                     </p>
-                    <p className="key">七日年化[12-19]</p>
+                    <p className="key">单位净值[12-20]</p>
                   </div>
                   <div className='d-wfsy'>
                     <p className="value">
                       <b className="percent">
-                        {rateData_top.reverse()[0]}<span>%</span>
+                        {rateData_top[rateData_top.length-1]}<span>%</span>
                       </b>
                     </p>
-                    <p className="key">七日年化[12-19]</p>
+                    <p className="key">累计净值[12-20]</p>
                   </div>
                   <ul className='data-display-rate-list'>
-                    <li className="data-display-rate-list-item">近1月: <span className="sp-rate">0.14%</span></li>
-                    <li className="data-display-rate-list-item">近3月: <span className="sp-rate">0.35%</span></li>
-                    <li className="data-display-rate-list-item">近6月: <span className="sp-rate">0.73%</span></li>
-                    <li className="data-display-rate-list-item">近1年: <span className="sp-rate">1.70%</span></li>
+                    <li className="data-display-rate-list-item">托管费：<span className="sp-rate">{fund.托管费}</span></li>
+                    <li className="data-display-rate-list-item">最高认购费：<span className="sp-rate">{fund.最高认购费}</span></li>
+                    <li className="data-display-rate-list-item">最高申购费：<span className="sp-rate">{fund.最高申购费}</span></li>
+                    <li className="data-display-rate-list-item">最高赎回费：<span className="sp-rate">{fund.最高赎回费}</span></li>
                   </ul>
                 </div>
                 <ul className='data-display-bottom'>
-                  <li className="data-display-bottom-item">基金规模: <span className="found-detail">32.63亿元</span></li>
-                  <li className="data-display-bottom-item">成立时间: <span className="found-detail">2017-08-03</span></li>
-                  <li className="data-display-bottom-item">基金经理: <span className="found-detail">邱骏</span></li>
-                  <li className="data-display-bottom-item">基金评级: <span className="found-detail">暂无评级</span></li>
-                  <li className="data-display-bottom-item">管理人:   <span className="found-detail">红土创新基金管理有限公司</span></li>
+                  <li className="data-display-bottom-item">基金规模: <span className="found-detail">{fund.份额规模}</span></li>
+                  <li className="data-display-bottom-item">成立时间: <span className="found-detail">{fund.成立日期}</span></li>
+                  <li className="data-display-bottom-item">基金经理: <span className="found-detail">{fund.基金经理}</span></li>
+                  <li className="data-display-bottom-item">基金类型: <span className="found-detail">{fund.基金类型}</span></li>
+                  <li className="data-display-bottom-item">管理人:   <span className="found-detail">{fund.基金管理人}</span></li>
                 </ul>
               </div>
               <div className="buy">
@@ -216,11 +270,11 @@ export default function FundsDetail() {
                   <p className="buy-item">申购状态：开放</p><p className="buy-item">赎回状态：开放</p>
                 </div>
                 <div className="tips">
-                  <p className="buy-item">费率: <span className="cost">0.00%</span></p>
+                  <p className="buy-item">费率: <span className="cost">{fund.托管费}</span></p>
                   <p className="buy-item"><span className="tips-i">按日计息</span></p>
                 </div>
               </div>
-              <a href={`https://fund.10jqka.com.cn/${foundID}/`}><Button type="primary">点此购买</Button></a>
+              <a href={`https://fund.10jqka.com.cn/${fundID}/`}><Button type="primary">点此购买</Button></a>
             </Col>
           </Row>
         </div>
